bacba57db89e3ca79ae4c4b21a0db98dc6279750
Haps <thiamhapstou@gmail.com>
2024-07-19 18:05:28 +0000

    ajout de la fonctionnalité qui permet de checker

--- js/produit.js
+++ js/produit.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getDatabase, ref, set, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getDatabase, ref, set, onValue, remove, get, update } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { format, parseISO, differenceInCalendarDays } from 'https://cdn.jsdelivr.net/npm/date-fns@2.23.0/esm/index.js';
 
@@ -17,6 +17,7 @@ const db = getDatabase(app);
 const auth = getAuth(app);
 
 let selectedDate = null;
+let userId = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -26,29 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
-            const userId = user.uid;
+            userId = user.uid;
 
             document.getElementById('productForm').addEventListener('submit', (e) => {
                 e.preventDefault();
 
                 if (validationForm()) {
-                    const productName = document.getElementById('nom').value;
-                    const productPrice = document.getElementById('prix_unitaire').value;
-                    const productQuantity = document.getElementById('quantite').value;
+                    const nom = document.getElementById('nom').value;
+                    const prix_unitaire = document.getElementById('prix_unitaire').value;
+                    const quantite = document.getElementById('quantite').value;
 
                     const productId = Date.now();
                     const productRef = ref(db, `products/${userId}/${selectedDate}/${productId}`);
 
                     set(productRef, {
-                        name: productName,
-                        price: productPrice,
-                        quantity: productQuantity
+                        name: nom,
+                        price: prix_unitaire,
+                        quantity: quantite
                     }).then(() => {
                         alert('Produit ajouté avec succès !');
                         document.getElementById('productForm').reset();
                         reset();
                         fetchProducts(userId, selectedDate);
-                        
                     }).catch((error) => {
                         console.error('Erreur lors de l\'ajout du produit : ', error);
                     });
@@ -67,7 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
 const validationForm = () => {
     let isValid = true;
 
-    // Validation du nom
     const nom = document.getElementById('nom');
     const valeurNom = nom.value.trim();
     const regexNoDigits = /^[^\d]+$/;
@@ -82,9 +81,8 @@ const validationForm = () => {
         setSuccess(nom);
     }
 
-    // Validation du prix
     const prix = document.getElementById('prix_unitaire');
-    const valeurPrix = prix.value.trim(); // Utilisation de trim() ici
+    const valeurPrix = prix.value.trim();
 
     if (valeurPrix === '') {
         setError(prix, 'Le prix est obligatoire');
@@ -96,9 +94,181 @@ const validationForm = () => {
         setSuccess(prix);
     }
 
-    // Validation de la quantité
     const quantite = document.getElementById('quantite');
-    const valeurQuantite = quantite.value.trim(); // Utilisation de trim() ici
+    const valeurQuantite = quantite.value.trim();
+
+    if (valeurQuantite === '') {
+        setError(quantite, 'La quantité est obligatoire');
+        isValid = false;
+    } else if (isNaN(parseInt(valeurQuantite, 10)) || parseInt(valeurQuantite, 10) <= 0) {
+        setError(quantite, 'La quantité doit être un nombre entier positif');
+        isValid = false;
+    } else {
+        setSuccess(quantite);
+    }
+
+    return isValid;
+};
+
+const reset = () => {
+    const nom = document.getElementById('nom');
+    const prix = document.getElementById('prix_unitaire');
+    const quantite = document.getElementById('quantite');
+  
+    nom.parentElement.classList.remove('success', 'error');
+    prix.parentElement.classList.remove('success', 'error');
+    quantite.parentElement.classList.remove('success', 'error');
+};
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+function fetchProducts(userId, date) {
+    const productsRef = ref(db, `products/${userId}/${date}`);
+    onValue(productsRef, (snapshot) => {
+        const productsTableBody = document.getElementById('productsTableBody');
+        productsTableBody.innerHTML = '';
+
+        snapshot.forEach((childSnapshot) => {
+            const product = childSnapshot.val();
+            const row = document.createElement('tr');
+            const statusClass = product.status === 'acheté' ? 'product-achete' : 'product-no-achete';
+
+
+            // Déterminer si l'icône de vérification doit être cachée
+            const checkIconStyle = product.status === 'acheté' ? 'display: none;' : '';
+
+
+            row.innerHTML = `
+                <td>${product.name}</td>
+                <td>${product.price}</td>
+                <td>${product.quantity}</td>
+                <td>
+                     <i class="material-icons" style="cursor: pointer; ${checkIconStyle}" onclick="checkProduct('${childSnapshot.key}')">check</i>
+                    <i class="material-icons" style="cursor: pointer;" onclick="editProduct('${childSnapshot.key}')">edit</i>
+                    <i class="material-icons" style="cursor: pointer;" onclick="deleteProduct('${userId}', '${date}', '${childSnapshot.key}')">delete</i>
+                </td>
+            `;
+            productsTableBody.appendChild(row);
+        });
+    });
+}
+
+function deleteProduct(userId, date, productId) {
+    if (confirm('Êtes-vous sûr de vouloir supprimer ce produit ?')) {
+        const productRef = ref(db, `products/${userId}/${date}/${productId}`);
+        
+        remove(productRef)
+            .then(() => {
+                console.log('Produit supprimé avec succès.');
+                fetchProducts(userId, date);
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la suppression du produit : ', error.message);
+            });
+    }
+}
+
+window.deleteProduct = deleteProduct;
+
+function editProduct(productId) {
+    if (!userId || !selectedDate) {
+        console.error('userId ou selectedDate non défini');
+        return;
+    }
+
+    document.getElementById('editProductForm').style.display = 'block';
+    document.getElementById('formAjout').style.display = 'none';
+
+    const productRef = ref(db, `products/${userId}/${selectedDate}/${productId}`);
+    get(productRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const product = snapshot.val();
+            document.getElementById('editProductId').value = productId;
+            document.getElementById('editnom').value = product.name;
+            document.getElementById('editprix_unitaire').value = product.price;
+            document.getElementById('editquantite').value = product.quantity;
+        } else {
+            console.error('Produit non trouvé.');
+        }
+    }).catch((error) => {
+        console.error('Erreur lors de la récupération des données : ', error.message);
+    });
+}
+
+window.editProduct = editProduct;
+
+document.getElementById('editProductForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (validationEditForm()) {
+        const productId = document.getElementById('editProductId').value;
+        const nom = document.getElementById('editnom').value;
+        const prix_unitaire = document.getElementById('editprix_unitaire').value;
+        const quantite = document.getElementById('editquantite').value;
+
+        const productRef = ref(db, `products/${userId}/${selectedDate}/${productId}`);
+
+        update(productRef, {
+            name: nom,
+            price: prix_unitaire,
+            quantity: quantite
+        }).then(() => {
+            alert('Produit mis à jour avec succès !');
+            document.getElementById('editProductForm').reset();
+            document.getElementById('editProductForm').style.display = 'none';
+            fetchProducts(userId, selectedDate);
+        }).catch((error) => {
+            console.error('Erreur lors de la mise à jour du produit : ', error.message);
+        });
+    }
+});
+
+function validationEditForm() {
+    let isValid = true;
+
+    const nom = document.getElementById('editnom');
+    const prix = document.getElementById('editprix_unitaire');
+    const quantite = document.getElementById('editquantite');
+
+    const valeurNom = nom.value.trim();
+    const valeurPrix = prix.value.trim();
+    const valeurQuantite = quantite.value.trim();
+
+    const regexNoDigits = /^[^\d]+$/;
+
+    if (valeurNom === '') {
+        setError(nom, 'Le nom est obligatoire');
+        isValid = false;
+    } else if (!regexNoDigits.test(valeurNom)) {
+        setError(nom, 'Le nom ne doit pas contenir de chiffres');
+        isValid = false;
+    } else {
+        setSuccess(nom);
+    }
+
+    if (valeurPrix === '') {
+        setError(prix, 'Le prix est obligatoire');
+        isValid = false;
+    } else if (isNaN(parseFloat(valeurPrix)) || parseFloat(valeurPrix) <= 0) {
+        setError(prix, 'Le prix doit être un nombre positif');
+        isValid = false;
+    } else {
+        setSuccess(prix);
+    }
 
     if (valeurQuantite === '') {
         setError(quantite, 'La quantité est obligatoire');
@@ -113,79 +283,43 @@ const validationForm = () => {
     return isValid;
 }
 
+function cancelEdit() {
+    document.getElementById('editProductForm').style.display = 'none';
+    document.getElementById('formAjout').style.display = 'block';
+}
 
-// Fonction pour réinitialiser les champs du formulaire
-const reset = () => {
-  
-    // Réinitialiser les classes de succès/erreur
-    nom.parentElement.classList.remove('success', 'error');
-    prix.parentElement.classList.remove('success', 'error');
-    quantite.parentElement.classList.remove('success', 'error');
-    
-  };
+window.cancelEdit = cancelEdit;
 
-// Fonction pour afficher un message d'erreur
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-  
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
-  }
-  
-  // Fonction pour afficher un message de succès
-  const setSuccess = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-  
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-  }
-  function fetchProducts(userId, date) {
-    const productsRef = ref(db, `products/${userId}/${date}`);
-    onValue(productsRef, (snapshot) => {
-        const productsTableBody = document.getElementById('productsTableBody');
-        productsTableBody.innerHTML = '';
+function checkProduct(productId) {
+    if (!userId || !selectedDate) {
+        console.error('userId ou selectedDate non défini');
+        return;
+    }
 
-        snapshot.forEach((childSnapshot) => {
-            const product = childSnapshot.val();
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${product.name}</td>
-                <td>${product.price}</td>
-                <td>${product.quantity}</td>
-                <td>
-                    <i class="material-icons" style="cursor: pointer;" onclick="checkProduct('${childSnapshot.key}')">check</i>
-                    <i class="material-icons" style="cursor: pointer;" onclick="editProduct('${childSnapshot.key}')">edit</i>
-                    <i class="material-icons" style="cursor: pointer;" onclick="deleteProduct('${userId}', '${date}', '${childSnapshot.key}')">delete</i>
-                </td>
-            `;
-            productsTableBody.appendChild(row);
-        });
+    const productRef = ref(db, `products/${userId}/${selectedDate}/${productId}`);
+
+    get(productRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const product = snapshot.val();
+            const newStatus = product.status === 'acheté' ? 'non-acheté' : 'acheté';
+
+            // Mettre à jour le statut du produit dans la base de données
+            update(productRef, { status: newStatus })
+                .then(() => {
+                    console.log('Statut du produit mis à jour avec succès.');
+                    fetchProducts(userId, selectedDate); // Réactualiser la liste des produits
+                })
+                .catch((error) => {
+                    console.error('Erreur lors de la mise à jour du produit : ', error.message);
+                });
+        } else {
+            console.error('Produit non trouvé.');
+        }
+    }).catch((error) => {
+        console.error('Erreur lors de la récupération des données : ', error.message);
     });
 }
-
-function deleteProduct(userId, date, productId) {
-    // Afficher une boîte de confirmation avant de supprimer
-    if (confirm('Êtes-vous sûr de vouloir supprimer ce produit ?')) {
-        const productRef = ref(db, `products/${userId}/${date}/${productId}`);
-        
-        // Supprimer le produit de Firebase
-        remove(productRef)
-            .then(() => {
-                console.log('Produit supprimé avec succès.');
-                // Rafraîchir la liste des produits après la suppression
-                fetchProducts(userId, date);
-            })
-            .catch((error) => {
-                console.error('Erreur lors de la suppression du produit : ', error.message);
-            });
-    }
-}
-
-window.deleteProduct = deleteProduct;
+window.checkProduct = checkProduct;
 function formatDate(dateStr) {
     const date = parseISO(dateStr);
     const today = new Date();
