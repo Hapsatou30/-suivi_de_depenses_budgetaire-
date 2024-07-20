d86322098d1a3b37ffb702bace5e17d0b64b79a2
Haps <thiamhapstou@gmail.com>
2024-07-20 20:13:03 +0000

    intégration de la page d'ajout de produit

--- js/produit.js
+++ js/produit.js
@@ -15,7 +15,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
-
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
+        mobileMenu.style.display = 'block';
+    } else {
+        mobileMenu.style.display = 'none';
+    }
+})
 let selectedDate = null;
 let userId = null;
 
@@ -31,21 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.getElementById('productForm').addEventListener('submit', (e) => {
                 e.preventDefault();
-
+            
                 if (validationForm()) {
                     const nom = document.getElementById('nom').value;
                     const prix_unitaire = document.getElementById('prix_unitaire').value;
                     const quantite = document.getElementById('quantite').value;
-
+            
                     const productId = Date.now();
                     const productRef = ref(db, `products/${userId}/${selectedDate}/${productId}`);
-
+            
                     set(productRef, {
                         name: nom,
                         price: prix_unitaire,
                         quantity: quantite
                     }).then(() => {
-                        alert('Produit ajouté avec succès !');
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Produit ajouté avec succès !',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
                         document.getElementById('productForm').reset();
                         reset();
                         fetchProducts(userId, selectedDate);
@@ -146,48 +158,36 @@ function fetchProducts(userId, date) {
             const product = childSnapshot.val();
             const row = document.createElement('tr');
             const statusClass = product.status === 'acheté' ? 'product-achete' : 'product-no-achete';
-
-
+        
             // Déterminer si l'icône de vérification doit être cachée
             const checkIconStyle = product.status === 'acheté' ? 'display: none;' : '';
-
-
+        
+            row.classList.add(statusClass);
+        
             row.innerHTML = `
                 <td>${product.name}</td>
                 <td>${product.price}</td>
                 <td>${product.quantity}</td>
-                <td>
-                     <i class="material-icons" style="cursor: pointer; ${checkIconStyle}" onclick="checkProduct('${childSnapshot.key}')">check</i>
-                    <i class="material-icons" style="cursor: pointer;" onclick="editProduct('${childSnapshot.key}')">edit</i>
-                    <i class="material-icons" style="cursor: pointer;" onclick="deleteProduct('${userId}', '${date}', '${childSnapshot.key}')">delete</i>
+                <td class="d-flex justify-content-between">
+                    <i class="material-icons" style="cursor: pointer; ${checkIconStyle} color:#8D2C5A;" onclick="checkProduct('${childSnapshot.key}')">add_task</i>
+                    <i class="material-icons" style="cursor: pointer; color:green;" onclick="editProduct('${childSnapshot.key}')">edit</i>
+                    <i class="material-icons" style="cursor: pointer; color:red;" onclick="deleteProduct('${userId}', '${date}', '${childSnapshot.key}')">delete</i>
                 </td>
             `;
             productsTableBody.appendChild(row);
         });
+        
     });
 }
 
-// function deleteProduct(userId, date, productId) {
-//     if (confirm('Êtes-vous sûr de vouloir supprimer ce produit ?')) {
-//         const productRef = ref(db, `products/${userId}/${date}/${productId}`);
-        
-//         remove(productRef)
-//             .then(() => {
-//                 console.log('Produit supprimé avec succès.');
-//                 fetchProducts(userId, date);
-//             })
-//             .catch((error) => {
-//                 console.error('Erreur lors de la suppression du produit : ', error.message);
-//             });
-//     }
-// }
+
 function deleteProduct(userId, date, productId) {
     Swal.fire({
         title: "Êtes-vous sûr ?",
         text: "Vous ne pourrez pas revenir en arrière !",
         icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
+        confirmButtonColor: "#8D2C5A",
         cancelButtonColor: "#d33",
         confirmButtonText: "Oui, supprimez-le !"
     }).then((result) => {
@@ -197,9 +197,10 @@ function deleteProduct(userId, date, productId) {
             remove(productRef)
                 .then(() => {
                     Swal.fire({
-                        title: "Supprimé !",
-                        text: "Votre produit a été supprimé.",
-                        icon: "success"
+                        icon: 'success',
+                            title: 'Produit supprimé avec succès !',
+                            showConfirmButton: false,
+                            timer: 1500
                     });
                     fetchProducts(userId, date);
                 })
@@ -332,15 +333,35 @@ function checkProduct(productId) {
             const product = snapshot.val();
             const newStatus = product.status === 'acheté' ? 'non-acheté' : 'acheté';
 
-            // Mettre à jour le statut du produit dans la base de données
-            update(productRef, { status: newStatus })
-                .then(() => {
-                    console.log('Statut du produit mis à jour avec succès.');
-                    fetchProducts(userId, selectedDate); // Réactualiser la liste des produits
-                })
-                .catch((error) => {
-                    console.error('Erreur lors de la mise à jour du produit : ', error.message);
-                });
+            Swal.fire({
+                title: "Êtes-vous sûr ?",
+                text: `Voulez-vous vraiment marquer ce produit comme ${newStatus} ?`,
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Oui, marquez-le !"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Mettre à jour le statut du produit dans la base de données
+                    update(productRef, { status: newStatus })
+                        .then(() => {
+                            console.log('Statut du produit mis à jour avec succès.');
+                            fetchProducts(userId, selectedDate); // Réactualiser la liste des produits
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Produit modifié avec succès !',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        })
+
+                        .catch((error) => {
+                            console.error('Erreur lors de la mise à jour du produit : ', error.message);
+                        });
+                }
+            });
+
         } else {
             console.error('Produit non trouvé.');
         }
@@ -348,6 +369,7 @@ function checkProduct(productId) {
         console.error('Erreur lors de la récupération des données : ', error.message);
     });
 }
+
 window.checkProduct = checkProduct;
 
 function formatDate(dateStr) {
